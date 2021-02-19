d82c4a5cd61c63b3f7dc97a45e24828ae5455424
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-02-19 20:31:53 +0100

    EjercicioEditFeatures_RCH

--- js/map.js
+++ js/map.js
@@ -9,11 +9,17 @@ require([
         "dojo/parser",
         "dojo/on",
         "dojo/_base/array",
+        "esri/layers/FeatureLayer",
+        "esri/tasks/GeometryService",
+        "esri/dijit/editing/Editor",
+        "esri/dijit/editing/TemplatePicker",
+        "esri/dijit/editing/AttachmentEditor",
+        "esri/undoManager",
 
         "dijit/layout/BorderContainer",
         "dijit/layout/ContentPane"],
     function (Map,
-              ready, parser, on, array,
+              ready, parser, on, array, FeatureLayer, GeometryService, Editor, TemplatePicker, AttachmentEditor, undoManager,
               BorderContainer, ContentPane) {
 // @formatter:on
 
@@ -39,7 +45,15 @@ require([
             /*
              * Step: Construct the editable layers
              */
-
+       flFirePoints = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Wildfire/FeatureServer/0", {
+                outFields: ['*']
+            });
+            flFireLines = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Wildfire/FeatureServer/1", {
+                outFields: ['*']
+            });
+            flFirePolygons = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Wildfire/FeatureServer/2", {
+                outFields: ['*']
+            });
 
             // Listen for the editable layers to finish loading
             mapMain.on("layers-add-result", initEditor);
@@ -60,25 +74,52 @@ require([
                  * Step: Map the event results into an array of Layer objects
                  */
 
+                var layersWildfire = array.map(results.layers, function (result) {
+                    return result.layer;
+                });
 
                 /*
                  * Step: Add a custom TemplatePicker widget
                  */
+                var tpCustom = new TemplatePicker({
+                    featureLayers: layersWildfire,
+                    columns: 2
+                }, "divLeft");
+                tpCustom.startup();
 
 
                 /*
                  * Step: Prepare the Editor widget settings
                  */
+                var editorSettings = {
+                    map: mapMain,
+                    geometryService: new GeometryService("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer"),
+                    layerInfos: layerInfosWildfire,
+                    toolbarVisible: true,
+                    templatePicker: tpCustom,
+                    createOptions: {
+                        polygonDrawTools: [Editor.CREATE_TOOL_FREEHAND_POLYGON, Editor.CREATE_TOOL_RECTANGLE, Editor.CREATE_TOOL_TRIANGLE, Editor.CREATE_TOOL_CIRCLE, Editor.CREATE_TOOL_ELLIPSE, Editor.CREATE_TOOL_AUTOCOMPLETE, Editor.CREATE_TOOL_ARROW]
+                    },
+                    toolbarOptions: {
+                        reshapeVisible: true
+                    },
+                    enableUndoRedo: true,
+                    maxUndoRedoOperations: 21
+                };
 
 
                 /*
                  * Step: Build the Editor constructor's first parameter
                  */
-
+                var editorParams = {
+                    settings: editorSettings
+                };
 
                 /*
                  * Step: Construct the Editor widget
                  */
+                widgetEditor = new Editor(editorParams, "divTop");
+                widgetEditor.startup();
 
 
             };
